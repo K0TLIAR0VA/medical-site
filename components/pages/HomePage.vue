d001e6602e4628469967faa4ec7e4c3c84ea1ad5
K0TLIAR0VA <1914820@gmail.com>
2019-10-30 17:04:53 +0200

    statr sattings

--- components/pages/HomePage.vue
+++ components/pages/HomePage.vue
@@ -1,3 +1,22 @@
 <template>
-    <div></div>
+  <div>
+    <Header></Header>
+
+    <Footer></Footer>
+  </div>
 </template>
+
+<script>
+import Header from "../pages_components/Header.vue";
+import Footer from "../pages_components/Footer.vue";
+export default {
+  components: {
+    Header: Header,
+    Footer: Footer
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
