59159e945f4b61255de428802b01e5df94095bd2
K0TLIAR0VA <1914820@gmail.com>
2019-11-16 10:04:56 +0200

    перенесла стили footer на sass

--- components/pages_components/Footer.vue
+++ components/pages_components/Footer.vue
@@ -3,32 +3,28 @@
     <div class="container">
       <img src="/img/logo/logo-white-with-title_v1.png" alt />
       <div class="footer-text">
-        <p>&copy; 2019 Особистий сайт лікаря-гінеколога Погорєлова Д.О.</p>
-        <p>Всі права захищено.</p>
-        <small>Developed by Kateryna</small>
+        <p class="white">&copy; 2019 Особистий сайт лікаря-гінеколога Погорєлова Д.О.</p>
+        <p class="white">Всі права захищено.</p>
+        <small class="white">Developed by Kateryna</small>
       </div>
     </div>
   </footer>
 </template>
 
-<style scoped>
-footer {
-  height: 25vh;
-  background: #7d3f98;
-}
-.container {
-  display: flex;
-  height: 100%;
-  justify-content: space-between;
-  align-items: center;
-}
-img {
-  display: block;
-  margin-left: 70px;
-}
-p, small {
-  color: #fff;
-  line-height: 2em;
-}
-small {font-size: 0.8em;}
+<style lang="sass" scoped>
+footer
+  height: 25vh
+  background: #7d3f98
+  .container
+    display: flex
+    height: 100%
+    justify-content: space-between
+    align-items: center
+    img
+      display: block
+      margin-left: 70px
+    p, small
+      line-height: 2em
+    small
+      font-size: 0.8em
 </style>
